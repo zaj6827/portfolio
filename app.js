54cd129f92fe4a809f21005473d167a196269f24
ZacharyJohnson <zaj6827@gmail.com>
2017-07-11 10:55:52 -0700

    Scaffolded and planned future displays through html comments

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var allProjects = [];
+
+function Project (preview, link) {
+  this.preview = preview;
+  this.link = link;
+  allProjects.push(this);
+  this.id = project;
+}
+
+//new Project(image,src?)
